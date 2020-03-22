99e15dafe6c466739f80986a51c1819cc2018f96
KostyaFedurov <kfedurov@gmail.com>
2020-03-22 13:08:24 +0300

    clear some unused files

---
+++ ToDoList/app/src/main/java/ru/fedurovkostya/todolist/Other/Const.kt
@@ -0,0 +1,11 @@
+package ru.fedurovkostya.todolist.Other
+
+const val DB_NAME = "ToDoList_main_9"
+const val TABLE_TASK = "Task"
+const val DB_VERSION = 1
+
+const val COL_ID = "id"
+const val COL_NAME = "name"
+const val COL_DESCRIPTION = "description"
+const val COL_COLOR = "color"
+const val COL_THEME = "theme"

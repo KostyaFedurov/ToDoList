7d810d3d7f560bc18392822702ed9e7e20a9bdf3
KostyaFedurov <kfedurov@gmail.com>
2020-03-28 21:15:59 +0300

    fb analytic + onesignal(real))))

---
+++ ToDoList/app/src/main/java/ru/fedurovkostya/todolist/ApplicationClass.java
@@ -0,0 +1,16 @@
+package ru.fedurovkostya.todolist;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
